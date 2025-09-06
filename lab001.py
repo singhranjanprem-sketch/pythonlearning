67a70022d36756a0c9829be9d2f94cf8bab45bbe
singhranjanprem-sketch <singhranjanprem@gmail.com>
2025-09-06 09:48:27 +0530

    Add files via upload

---
+++ lab001.py
@@ -0,0 +1,10 @@
+name=input("Enter your name")
+print(name)
+
+
+Firstname=input("Enter your First name")
+Lastname=input("Enter your Last name")
+print(Firstname,Lastname,sep="-",end="\t")
+
+name=input("Enter your name")
+print("You are welcome.",name,"to the Python Class.")
